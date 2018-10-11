776105db86647eb44842af659f3c065311fd7a83
Bankim Bhavsar <bankim@cloudera.com>
2018-10-11 16:13:25 -0700

    Transform word core impl

--- transform_word.cc
+++ transform_word.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 #include <unordered_map>
 #include <unordered_set>
 #include <string>
@@ -70,12 +71,68 @@ public:
             cout << endl;
         }
     }
+
+    vector<string> transform_src_dst(const string& src, const string& dst) {
+      vector<string> result;
+      // src word doesn't exist in dictionary.
+      if (dict.count(src) == 0) {
+        return result;
+
+      }
+      unordered_set<string> visited;
+      unordered_map<string, string> rev_map;
+      list<string> q;
+      q.push_back(src);
+      string u;
+      while (!q.empty()) {
+        u = q.front();
+        q.pop_front();
+        if (u == dst) {
+          break;
+        }
+        for (auto v : graph[u]) {
+          if (visited.count(v) == 0) {
+            visited.insert(v);
+            q.push_back(v);
+            rev_map.insert(make_pair(v, u));
+          }
+        }
+      }
+
+      if (u == dst) {
+        string curr = dst;
+        while (curr != src) {
+          result.push_back(curr);
+          curr = rev_map[curr];
+        }
+        result.push_back(curr);
+        reverse(result.begin(), result.end());
+      }
+
+      return result;
+    }
 };
 
+void transform_helper(WordTransform& wt, const string& src, const string& dst) {
+    vector<string> result = wt.transform_src_dst(src, dst);
+    for (auto s : result) {
+      cout << s << " ";
+    }
+    cout << endl;
+}
+
+
 int main() {
-    vector<string> words = {"hi", "hello", "ola", "hit", "he", "hell", "ol", "hits"};
+    vector<string> words = {"hi", "hello", "ola", "hit", "he", "hell", "ol", "hits", "cola", "colas"};
     WordTransform wt(words);
-    wt.print_graph();
+    // wt.print_graph();
+
+    pair<string, string> pairs[] = { make_pair("he", "hits"), make_pair("hell", "hello"),
+      make_pair("colas", "ol")};
+
+    for (auto p : pairs) {
+      transform_helper(wt, p.first, p.second);
+    }
 
     return 0;
 }
