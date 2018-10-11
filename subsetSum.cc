70b8d70dd2f23a34fca6ddf5aece5e526c776d43
Bankim Bhavsar <bankim@cloudera.com>
2018-10-11 06:18:43 -0700

    Subset sum with DP

--- subsetSum.cc
+++ subsetSum.cc
@@ -1,8 +1,54 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <assert.h>
 
 using namespace std;
+
+bool subset_dp(const vector<int>& vec, int target, vector<int>& result) {
+
+    bool dp[vec.size() + 1][target + 1];
+    int incl[vec.size() + 1][target + 1];
+
+    for (int col = 1; col <= target; col++) {
+        dp[0][col] = false;
+    }
+    for (int row = 1; row <= vec.size(); row++) {
+        dp[row][0] = false;
+    }
+
+    dp[0][0] = true;
+    for (int i = 1; i <= vec.size(); i++) {
+        for (int s = 1; s <= target; s++) {
+            if (s - vec[i-1] < 0) {
+                dp[i][s] = dp[i-1][s];
+            } else {
+                bool yes = dp[i-1][s - vec[i-1]];
+                if (yes) {
+                    incl[i][s] = vec[i-1];
+                } else {
+                    incl[i][s] = -1;
+                }
+                dp[i][s] = dp[i-1][s] || dp[i-1][s - vec[i-1]];
+            }
+        }
+    }
+
+    if (!dp[vec.size()][target]) {
+        return false;
+    }
+
+    int s = target;
+    for (int i = vec.size(); i > 0; i--) {
+        if (incl[i][s] != -1) {
+            result.push_back(incl[i][s]);
+            s -= incl[i][s];
+        }
+    }
+    assert(s == 0);
+
+    return true;
+}
 
 
 void getSetsRecurse(const vector<int>& vec, int target, int pos, int currSum,
@@ -48,6 +94,17 @@ int main() {
         }
         cout << endl;
     }
+    cout << endl;
+
+    vector<int> result;
+    bool yes = subset_dp(input, 6, result);
+    cout << yes << endl;
+    for (int elem : result) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
     return 0;
+
 }
 
