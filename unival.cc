41dc440a4c126ad176dca483cdf013e0bc2bc68b
Bankim Bhavsar <bankim@cloudera.com>
2019-03-31 21:09:38 -0700

    Correct code for unival tree

--- unival.cc
+++ unival.cc
@@ -1,24 +1,29 @@
 #include "common.hh"
 #include "bt.hh"
 
-bool unival(TreeNode *curr, int parent_val, int& num) {
+bool unival(TreeNode *curr, int& num) {
     if (curr == NULL) {
         return true;
     }
 
-    bool left = unival(curr->left, curr->val, num);
-    bool right = unival(curr->right, curr->val, num);
+    bool left = unival(curr->left, num);
+    bool right = unival(curr->right, num);
 
-    if (left && right) {
-        num++;
-    }
-    if (parent_val == curr->val && left && right) {
-        return true;
-    } else {
+    if (!left || !right) {
         return false;
     }
-}
 
+    if (curr->left && curr->left->val != curr->val) {
+        return false;
+    }
+
+    if (curr->right && curr->right->val != curr->val) {
+        return false;
+    }
+
+    num++;
+    return true;
+}
 
 int main() {
     vector<int> v = {50, 25, 75, 20, 30, 100, 125};
@@ -46,12 +51,12 @@ int main() {
     TreeNode *r = new TreeNode(0, new TreeNode(1), new TreeNode(0, new TreeNode(1, new TreeNode(1), new TreeNode(1)), new TreeNode(0)));
 
     int num = 0;
-    unival(r, r->val, num);
+    unival(r, num);
     cout << num << endl;
 
     TreeNode *n = new TreeNode('a', new TreeNode('c'), new TreeNode('b', new TreeNode('b'), new TreeNode('b', NULL, new TreeNode('b'))));
     num = 0;
-    unival(n, n->val, num);
+    unival(n, num);
     cout << num << endl;
 
     return 0;
