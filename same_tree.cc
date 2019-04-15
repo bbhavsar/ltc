dfdf0f81ddb9c09617b7548081c36db5d165970a
Bankim Bhavsar <bankim@cloudera.com>
2019-04-15 10:20:09 -0700

    Same tree

---
+++ same_tree.cc
@@ -0,0 +1,34 @@
+/*
+https://leetcode.com/problems/same-tree/
+
+*/
+
+#include "common.hh"
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (p == NULL && q == NULL) {
+        return true;
+    }
+    if ((p == NULL) != (q == NULL)) {
+        return false;
+    }
+    if (p->val != q->val) {
+        return false;
+    }
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
+int main() {
+
+    return 0;
+}
+
