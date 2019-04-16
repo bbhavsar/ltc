33072269e19dd276345b1ae447a214887a905b6d
Bankim Bhavsar <bankim@cloudera.com>
2019-04-16 15:41:52 -0700

    Preorder with iterator class

--- preorder.cc
+++ preorder.cc
@@ -8,6 +8,35 @@ struct TreeNode {
     TreeNode(int v, TreeNode *l = NULL, TreeNode* r = NULL) : val(v), left(l), right(r) {}
 };
 
+class Iterator {
+private:
+    stack<TreeNode *> s;
+public:
+    Iterator(TreeNode *r)  {
+        if (r == NULL) {
+            return;
+        }
+        s.push(r);
+    }
+    TreeNode *next() {
+        TreeNode *result = NULL;
+
+        if (!s.empty()) {
+            TreeNode *curr = s.top();
+            s.pop();
+            assert(curr != NULL);
+            result = curr;
+            if (curr->right != NULL) {
+                s.push(curr->right);
+            }
+            if (curr->left != NULL) {
+                s.push(curr->left);
+            }
+        }
+        return result;
+    }
+};
+
 
 void preorder(TreeNode *root) {
     if (root == NULL) {
@@ -18,9 +47,8 @@ void preorder(TreeNode *root) {
     while (!s.empty()) {
         root = s.top();
         s.pop();
-        cout << root->val << " ";
-        while (root->left != NULL) {
-            cout << root->left->val << " ";
+        while (root != NULL) {
+            cout << root->val << " ";
             if (root->right != NULL) {
                 s.push(root->right);
             }
@@ -37,6 +65,11 @@ int main() {
                                        new TreeNode(70, new TreeNode(60), new TreeNode(80)));
 
     preorder(root);
+    Iterator it(root);
+    TreeNode *next;
+    while ((next = it.next()) != NULL ) {
+        cout << next->val << " ";
+    }
 
     return 0;
 }
